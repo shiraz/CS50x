34a33b774eb36e66ab37ee5558b905948fc5f08f
mirzasb <mirzasbaig@nyu.edu>
2021-12-22 17:49:53 -0500

    Error handling for tax calculate API

--- project/pages/api/tax/calculate.js
+++ project/pages/api/tax/calculate.js
@@ -6,6 +6,41 @@ async function handler(req, res) {
       body: { exemptions, filingStatus, payPeriod, payRate, state },
     } = req;
 
+    if (!exemptions) {
+      res.status(400).json({
+        status: 400,
+        message: "The 'exemptions' vallue cannot be empty.",
+      });
+    }
+
+    if (!filingStatus) {
+      res.status(400).json({
+        status: 400,
+        message: "The 'filingStatus' vallue cannot be empty.",
+      });
+    }
+
+    if (!payPeriod) {
+      res.status(400).json({
+        status: 400,
+        message: "The 'payPeriod' vallue cannot be empty.",
+      });
+    }
+
+    if (!payRate) {
+      res.status(400).json({
+        status: 400,
+        message: "The 'payRate' vallue cannot be empty.",
+      });
+    }
+
+    if (!state) {
+      res.status(400).json({
+        status: 400,
+        message: "The 'state' vallue cannot be empty.",
+      });
+    }
+
     const payPeriodNum = getPayPeriod(payPeriod);
 
     if (payPeriodNum == 0) {
